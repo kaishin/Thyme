bb01806442653ab6f0520f2f569c402db82493ac
Reda Lemeden <reda@thoughtbot.com>
2015-06-13 01:40:46 -0700

    Add UIBezierPath extension

---
+++ Thyme/UIBezierPath.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension UIBezierPath {
+  /// Fills a path with a given color.
+  ///
+  /// :param: color The color to use for filling the path.
+  func fill(#color: UIColor) {
+    color.setFill()
+    self.fill()
+    UIColor.clearColor().setFill()
+  }
+}
