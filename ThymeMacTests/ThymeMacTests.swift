c578665db7a1f4735ca553a5006b2b0f865243b7
Reda Lemeden <reda@thoughtbot.com>
2015-06-13 22:55:02 +0200

    Back to green

--- ThymeMacTests/ThymeMacTests.swift
+++ ThymeMacTests/ThymeMacTests.swift
@@ -1,6 +1,7 @@
 import Quick
 import Nimble
 import Thyme
+import AppKit
 
 class UIImageExtensionSpec: QuickSpec {
   override func spec() {
--- ThymeTests/ThymeTests.swift
+++ ThymeTests/ThymeTests.swift
@@ -1,6 +1,7 @@
 import Quick
 import Nimble
 import Thyme
+import UIKit
 
 class UIImageExtensionSpec: QuickSpec {
   override func spec() {
