10a185d5f5a3ea397665a98bd0edf82782e03459
Reda Lemeden <reda@thoughtbot.com>
2015-06-21 15:45:20 +0200

    Add CGRect corner point helpers
    - This allows you to get the coordinates of corner points regardless of the platform.

--- Shared/Extensions/CGRect.swift
+++ Shared/Extensions/CGRect.swift
@@ -6,4 +6,64 @@ public extension CGRect {
     self.size = size
     self.origin = CGPoint(x: center.x - (size.width / 2), y: center.y - (size.height / 2))
   }
+
+  /// Returns a CGPoint instance corresponding to the top left corner of the rect regardless of the platfrom.
+  public var topLeftPoint: CGPoint {
+    get {
+      var point = CGPointZero
+
+      executeOn(iOS: {
+        point = self.origin
+      }, mac: {
+        point = CGPoint(x: 0.0, y: self.height)
+      })
+
+      return point
+    }
+  }
+
+  /// Returns a CGPoint instance corresponding to the bottom left corner of the rect regardless of the platfrom.
+  public var bottomLeftPoint: CGPoint {
+    get {
+      var point = CGPointZero
+
+      executeOn(iOS: {
+        point = CGPoint(x: 0.0, y: self.height)
+      }, mac: {
+        point = self.origin
+      })
+
+      return point
+    }
+  }
+
+  /// Returns a CGPoint instance corresponding to the top right corner of the rect regardless of the platfrom.
+  public var topRightPoint: CGPoint {
+    get {
+      var point = CGPointZero
+
+      executeOn(iOS: {
+        point = CGPoint(x: self.width, y: 0.0)
+      }, mac: {
+        point = CGPoint(x: self.width, y: self.height)
+      })
+
+      return point
+    }
+  }
+
+  /// Returns a CGPoint instance corresponding to the bottom right corner of the rect regardless of the platfrom.
+  public var bottomRightPoint: CGPoint {
+    get {
+      var point = CGPointZero
+
+      executeOn(iOS: {
+        point = CGPoint(x: self.width, y: self.height)
+      }, mac: {
+        point = CGPoint(x: self.width, y: 0)
+      })
+
+      return point
+    }
+  }
 }
---
+++ ThymeMacTests/CGRectExtensionSpec.swift
@@ -0,0 +1,34 @@
+import Quick
+import Nimble
+import Thyme
+import AppKit
+
+class CGRectExtensionSpec: QuickSpec {
+  override func spec() {
+    describe("CGRectExtension") {
+      let rect = CGRect(origin: CGPointZero, size: CGSize(width: 100, height: 100))
+
+      it("should create a rect using a center point instead of an origin") {
+        let centerPoint = CGPoint(x: 50, y: 50)
+        let centeredRect = CGRect(center: centerPoint, size: CGSize(width: 100, height: 100))
+        expect(centeredRect.origin).to(equal(CGPointZero))
+      }
+
+      it("should return the top left point of the rect") {
+        expect(rect.topLeftPoint).to(equal(CGPoint(x: 0, y: rect.height)))
+      }
+
+      it("should return the top right point of the rect") {
+        expect(rect.topRightPoint).to(equal(CGPoint(x: rect.width, y: rect.height)))
+      }
+
+      it("should return the bottom right point of the rect") {
+        expect(rect.bottomRightPoint).to(equal(CGPoint(x: rect.width, y: 0)))
+      }
+
+      it("should return the bottom left point of the rect") {
+        expect(rect.bottomLeftPoint).to(equal(CGPointZero))
+      }
+    }
+  }
+}
